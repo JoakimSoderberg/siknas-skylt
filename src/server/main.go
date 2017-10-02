c07772d4524f5e2c556d63efc1282b9d79898de4
Joakim Söderberg <joakim.soderberg@gmail.com>
2017-10-03 00:33:52 +0200

    Some small changes

--- src/server/main.go
+++ src/server/main.go
@@ -8,19 +8,25 @@ import (
     "github.com/gobwas/ws/wsutil"
 )
 
+func ws_listener() {
+}
+
 func main() {
     fmt.Printf("Starting")
     http.ListenAndServe(":8080", http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
         conn, _, _, err := ws.UpgradeHTTP(r, w, nil)
         if err != nil {
-            // handle error
+            fmt.Printf("Failed to Ugrade websocket connection")
+            conn.Close()
+            return
         }
 
         go func() {
             defer conn.Close()
 
+            fmt.Printf("New Websocket Client\n")
+
             for {
-                fmt.Printf("Msg")
                 msg, op, err := wsutil.ReadClientData(conn)
                 if err != nil {
                     // handle error
