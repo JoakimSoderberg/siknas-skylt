a66def093e997a0c53da3eaa5ef1f319c06d8b32
Joakim Söderberg <joakim.soderberg@gmail.com>
2017-10-03 01:15:42 +0200

    Add control panel ws listener and todos

--- src/server/main.go
+++ src/server/main.go
@@ -11,6 +11,36 @@ import (
 	"github.com/gorilla/mux"
 )
 
+func control_panel_ws_listener(w http.ResponseWriter, r *http.Request) {
+	conn, _, _, err := ws.UpgradeHTTP(r, w, nil)
+	if err != nil {
+		fmt.Printf("Failed to Ugrade websocket connection\n")
+		conn.Close()
+		return
+	}
+
+	// TODO: Only allow one client.
+
+	go func() {
+		defer conn.Close()
+
+		fmt.Printf("New Websocket Client\n")
+
+		for {
+			msg, op, err := wsutil.ReadClientData(conn)
+			if err != nil {
+				// handle error
+			}
+			// TODO: Broadcast control panel messages to ws_listener
+			/*
+				err = wsutil.WriteServerMessage(conn, op, msg)
+				if err != nil {
+					// handle error
+				}*/
+		}
+	}()
+}
+
 func ws_listener(w http.ResponseWriter, r *http.Request) {
 	conn, _, _, err := ws.UpgradeHTTP(r, w, nil)
 	if err != nil {
@@ -39,6 +69,7 @@ func ws_listener(w http.ResponseWriter, r *http.Request) {
 
 func index(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "Hello, %q", html.EscapeString(r.URL.Path))
+	// TODO: Host aurelia webpage
 }
 
 func main() {
@@ -47,5 +78,7 @@ func main() {
 	router := mux.NewRouter().StrictSlash(true)
 	router.HandleFunc("/", index)
 	router.HandleFunc("/ws", ws_listener)
+	router.HandleFunc("/ws/control_panel", control_panel_ws_listener)
+
 	log.Fatal(http.ListenAndServe(":8080", router))
 }
