75ba52e9b7074275e60e9f0fe37f7a7a53562ee8
Joakim Söderberg <joakim.soderberg@gmail.com>
2017-11-02 22:48:54 +0100

    Add D3

---
+++ src/server/static/siknas-skylt/src/siknas-skylt.ts
@@ -0,0 +1,12 @@
+import { autoinject } from 'aurelia-framework';
+import * as d3 from "d3";
+
+@autoinject()
+export class SiknasSkylt {
+
+    constructor(private d: d3) {
+        
+    }
+
+    
+}
