c5bc11625d09f8d32394b1e827c2fa15d97d1e44
Joakim Söderberg <joakim.soderberg@gmail.com>
2017-11-03 16:18:50 +0100

    Some cleanup

--- src/server/static/siknas-skylt/src/siknas-skylt.ts
+++ src/server/static/siknas-skylt/src/siknas-skylt.ts
@@ -12,32 +12,31 @@ interface OPCPixel {
     color: OPCPoint;
 }
 
-interface OPCColor {
-    r: number;
-    g: number;
-    b: number;
-}
-
 const OPC_HEADER_LEN = 4;
 
 @autoinject()
 export class SiknasSkylt {
     svg: any;
     skylt: any;
+    // TODO: make it possible to disable the animation via a bindable property.
 
     layout: OPCPixel[];
+    pixelsCreated: boolean = false;
     pixels: any;
 
     constructor(private opc: WSOPCAPI, private events: EventAggregator, private client: HttpClient) {
         this.events.subscribe(WebsocketOPCMessage, msg => {
+            if (!this.pixelsCreated)
+                return;
+
             let d = new Uint8Array(msg.data);
             let channel = d[0];
             let command = d[1];
             let highLen = d[2];
             let lowLen = d[3];
             let length = (highLen << 8) | lowLen;
-            console.log("length: " + length + " length layout: " + this.layout.length);
 
+            // TODO: Would be good if we could set the style directly instead of first copying here...
             for (let i = OPC_HEADER_LEN, j = 0; i < length; i += 3, j++) {
                 let r = d[i]
                 let g = d[i + 1]
@@ -64,9 +63,24 @@ export class SiknasSkylt {
         let w = parseInt(this.svg.style("width"), 10);
         let h = parseInt(this.svg.style("height"), 10);
 
+        var defs = this.svg.append("defs");
+        var filter = defs.append("filter")
+            .attr("id", "glow")
+            .append("feGaussianBlur")
+            .attr("stdDeviation", "0.5")
+            .attr("result", "coloredBlur");
+
+        // Merge the original shape with the blur.
+        var feMerge = filter.append("feMerge");
+        feMerge.append("feMergeNode")
+            .attr("in", "coloredBlur");
+        feMerge.append("feMergeNode")
+            .attr("in", "SourceGraphic");
+
         this.pixels = this.svg.selectAll("circle").data(this.layout)
             .enter()
             .append("circle")
+            // These values are hand tweaked to be placed over the logo properly.
             .attr("cx", function (p: OPCPixel) {
                 return (p.point[0] * w * 0.81) + (w * 0.05);
             })
@@ -74,13 +88,14 @@ export class SiknasSkylt {
                 return (p.point[1] * h * 0.55) + (h * 0.20);
             })
             .attr("r", 2)
+            .attr("filter", "url(#glow)")
             .style("fill", function (p: OPCPixel) {
                 return `rgb(${p.color[0]},${p.color[1]},${p.color[2]})`
             });
-        //.style("stroke", "black")
-        //.style("stroke-width", 1.0);
 
         this.pixels.exit().remove();
+
+        this.pixelsCreated = true;
     }
 
     attached() {
