ac8ed0bbbae8a5506bc0ab6b616292b344408503
Joakim Soderberg <joakim.soderberg@netrounds.com>
2018-05-19 18:11:33 +0200

    Put brightness at bottom as navbar

--- src/server/static/siknas-skylt/src/app.ts
+++ src/server/static/siknas-skylt/src/app.ts
@@ -1,26 +1,12 @@
-import { Router, RouterConfiguration } from 'aurelia-router';
 import { autoinject, observable } from 'aurelia-framework';
 import { WSAPI } from './ws-api';
 
 @autoinject()
 export class App {
-  router: Router;
-
   constructor(private api: WSAPI) { }
 
-  blurValue: number = 10;
+  blurValue: number = 9;
   @observable brightness: number = 128;
-
-  /*
-  configureRouter(config: RouterConfiguration, router: Router) {
-    config.title = 'Animationer';
-    config.map([
-      { route: '', moduleId: 'no-selection', title: 'Select' },
-      { route: 'animations/:name',  moduleId: 'animation-detail', name:'animations' }
-    ]);
-
-    this.router = router;
-  }*/
 
   brightnessChanged(newValue, oldValue) {
     this.api.sendBrightnessMessage(parseInt(newValue));
@@ -29,5 +15,10 @@ export class App {
   created() {
     this.api.connect();
     // TODO: Add button to turn off animation.
+  }
+
+  stop() {
+    console.log("Stoppings");
+    this.api.sendPlayMessage("");
   }
 }
